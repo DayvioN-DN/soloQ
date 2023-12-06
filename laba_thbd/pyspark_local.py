5ac26eb4dfc468aadb53ff4b9b7cf675e8cbfd54
artem <DayvioN-DN@yandex.ru>
2023-12-06 16:08:49 +0300

    Сделал измерение по времени при изменении стобцов

--- laba_thbd/pyspark_local.py
+++ laba_thbd/pyspark_local.py
@@ -2,6 +2,8 @@ from pyspark.sql import SparkSession
 from pyspark import SparkConf
 from pyspark.sql.functions import col, collect_list
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+from pyspark.sql.functions import expr
+
 import time
 
 conf = SparkConf().set("spark.driver.memory", "8g")
@@ -12,16 +14,36 @@ spark = SparkSession.builder\
         .appName('PySpark_Tutorial')\
         .getOrCreate()
 
+
 df = spark.read.csv("custom_1988_2020.csv", sep=",", inferSchema=True, header=True)
 
+sum_time = 0
+for i in range(100):
+        cur_time = time.time()
+        df2 = df.withColumn("34353", col('34353')**3)
+
+        sum_time = sum_time + time.time() - cur_time
+print(sum_time/100)
 df.write.parquet("./output/people.parquet")
 df.write.orc("./output/people.orc")
 parDF=spark.read.parquet("./output/people.parquet")
 orcDF=spark.read.orc("./output/people.orc")
-parDF.show(1000000)
-orcDF.show(1000000)
+sum_time = 0
+for i in range(100):
+        cur_time = time.time()
+        df2 = parDF.withColumn("34353", col('34353')**3)
 
-print('yes')
+        sum_time = sum_time + time.time() - cur_time
+print(sum_time/100)
+sum_time = 0
+for i in range(100):
+        cur_time = time.time()
+        df3 = orcDF.withColumn("34353", col('34353')**3)
+
+        sum_time = sum_time + time.time() - cur_time
+print(sum_time/100)
+
+
 
 
 
